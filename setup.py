6fa10c4847876551ca73e45b79c0de25d06cdc19
Gallagher Donovan Pryor <gallagher.pryor@gmail.com>
2020-12-24 08:04:47 -0500

    setup.py: drop post-install build steps

--- setup.py
+++ setup.py
@@ -18,36 +18,6 @@ def read_package_variable(key):
     raise KeyError("'{0}' not found in '{1}'".format(key, module_path))
 
 
-def install_arrayfire():
-    arrayfire_version = '3.6.2'
-    arrayfire_filename = f'ArrayFire-v{arrayfire_version}_Linux_x86_64.sh'
-
-    check_call('mkdir -p /opt/arrayfire'.split())
-
-    arrayfire_predownloaded_filename = os.path.join('/mnt', 'd3m', arrayfire_filename)
-    if os.path.exists(arrayfire_predownloaded_filename):
-        arrayfire_downloaded_filename = arrayfire_predownloaded_filename
-    else:
-        arrayfire_downloaded_filename = os.path.join('/tmp', arrayfire_filename)
-        check_call((f'wget -P /tmp https://arrayfire.s3.amazonaws.com/{arrayfire_version}/ArrayFire-v{arrayfire_version}_Linux_x86_64.sh').split())
-    check_call((f'bash {arrayfire_downloaded_filename} --skip-license --prefix=/opt/arrayfire').split())
-
-    check_call(['bash', '-c', 'echo \'export LD_LIBRARY_PATH=/opt/arrayfire/lib64:$LD_LIBRARY_PATH\' >> ~/.bashrc'])
-    check_call(['bash', '-c', 'echo \'export LD_PRELOAD=/usr/local/lib/libmkl_core.so:/usr/local/lib/libmkl_sequential.so\' >> ~/.bashrc'])
-
-
-class PostDevelopStep(develop):
-    def run(self):
-        install_arrayfire()
-        develop.run(self)
-
-
-class PostInstallStep(install):
-    def run(self):
-        install_arrayfire()
-        install.run(self)
-
-
 setup(
     name=PACKAGE_NAME,
     version=read_package_variable('__version__'),
@@ -66,9 +36,5 @@ setup(
             'classification.logistic_regression.ArrayFire = af_primitives.af_LogisticRegression:af_LogisticRegression',
             'classification.k_neighbors.ArrayFire = af_primitives.af_KNeigborsClassifier:af_KNeighborsClassifier'
         ],
-    },
-    cmdclass={
-        'develop': PostDevelopStep,
-        'install': PostInstallStep,
-    },
+    }
 )
